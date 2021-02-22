35487df0a93a2fd72bbe05b9f943f86710e3351d
AleksandarStojsavljevic <aleksandarstojsavljevic@gmail.com>
2021-02-22 15:45:00 +0100

    level2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let original = {
   name: "Sale",
-  address: "Arsenija Carnojevica 77/17",
-  city: "Subotica",
-  zipCode: 24000,
-  country: "Serbia",
+  address: {
+    street: "Arsenija Carnojevica 77/17",
+    city: "Subotica",
+    zipCode: 24000,
+    country: "Serbia",
+  },
 };
 
 console.log("original", original);
@@ -12,7 +14,7 @@ let copied = { ...original };
 
 console.log("copied", copied);
 
-copied.name = "Damir";
+copied.address.city = "Beograd";
 
 console.log("original after change", original);
 console.log("copied after change", copied);
